124993427209d973f3cac0eaf2053e200c763046
Yidong Xia <yidong.xia@inl.gov>
2017-12-29 23:22:21 -0700

    Added IMG2VOX -- a MATLAB script

--- inl/utils/img2vox/img2vox.m
+++ inl/utils/img2vox/img2vox.m
@@ -1,109 +1,241 @@
-%-- 12/18/2017 05:13:29 PM --%
-Open3DStack
-clc
-Open3DStack
-CC=bwconncomp(BW3D,26)
-NumVox=sort(cellfun(@numel,CC.PixelIdxList));
-NumVox(1:20)
-NumVox=sort(cellfun(@numel,CC.PixelIdxList),'descend');
-NumVox(1:20)
-sum(sum(sum(BW3D)))
-100*NumVox(1:20)/ans
-histogram(100*NumVox/sum(sum(sum(BW3D))))
-100*NumVox(end-20:end)/sum(sum(sum(BW3D)))
-0.2085*sum(sum(sum(BW3D)))
-0.2085*sum(sum(sum(BW3D)))/100
-numel(BW3D)
-clc
-[~,Idx]=sort(cellfun(@numel,CC.PixelIdxList),'descend');
-[R,C,Z]=ind2sub(size(BW3D),CC.PixelIdxList{Idx(1)});
-mean(R)
-mean(C)
-mean(Z)
-LargePore=false(size(BW3D));
-LargePore(CC.PixelIdxList{Idx(1)})=true;
-subplot(1,2,1)
-imshow(BW3D(:,:,1))
-subplot(1,2,2)
-imshow(LargePore(:,:,1))
-subplot(1,2,1)
-imshow(BW3D(:,:,end))
-subplot(1,2,2)
-imshow(LargePore(:,:,end))
-subplot(2,2,4)
-imshow(LargePore(:,:,1))
-min(R)
-max(R)
-min(C)
-max(C)
-min(Z)
-max(Z)
-LargePoreBB=LargePore(min(R):max(R),min(C):max(C),min(Z):max(Z));
-scatter3(R,C,Z,'.')
-scatter3(R,C,Z,'.',0.5)
-scatter3(R,C,Z,'.',1)
-clc
-scatter3(R,C,Z,'.')
-hold on
-for i=1:10
-[R,C,Z]=ind2sub(size(BW3D),CC.PixelIdxList{Idx(i)});
-scatter3(R,C,Z,,'.')
+fprintf('#############################################################\n');
+fprintf('#                                                           #\n');
+fprintf('# IMG2VOX - a MATLAB code to convert stack images to voxels #\n');
+fprintf('#                                                           #\n');
+fprintf('# by Yidong Xia, Joshua Kane (Idaho National Laboratory)    #\n');
+fprintf('#                                                           #\n');
+fprintf('# This code does the following tasks:                       #\n');
+fprintf('#   * convert stack images to voxel data                    #\n');
+fprintf('#   * porosity distribution analysis                        #\n');
+fprintf('#                                                           #\n');
+fprintf('#############################################################\n');
+
+%{
+Open a MATLAB built-in GUI to look for image files.
+    * Some specific image types are listed.
+    * See help menu for all matlab recognized image types.
+    * MultiSelect allows the user to select multiple images.
+
+Variables defined in the function
+     fileName: the actual name of the file, e.g. "example.jpg";
+               has class cell
+     pathName: the file path, e.g. "C:\Users\KaneJJ\Desktop\";
+               has class string
+%}
+[fileName, pathName, ~] =...
+    uigetfile(...
+        {'*.jpg;*.tif;*.tiff;*.png;*.gif;*.bmp;','All Image Files'},...
+        'Select stack images to be processed',...
+        'MultiSelect','on'...
+    );
+
+%{
+Determine the number of files to be processed.
+    * nFiles must be >= 0 and <= 2^16-1
+%}
+nFiles = uint16( size(fileName, 2) );
+
+%{
+Convert file name from cell to string.
+%}
+fileName = char(fileName);
+
+%{
+Get the pixel resolution of each image
+%}
+img = imread( [pathName fileName(1,:)] );
+imgSize = size(img);
+
+%{
+Initiate a 3D matrix for the stack images
+%}
+if ( isa(img, 'double') )
+    BW3D = zeros( imgSize(1), imgSize(2), nFiles );
+elseif ( isa(img, 'single') )
+    BW3D = zeros( imgSize(1), imgSize(2), nFiles, 'single' );
+elseif ( isa(img, 'uint16') )
+    BW3D = zeros( imgSize(1), imgSize(2), nFiles, 'uint16' );
+elseif ( isa(img, 'uint8') )
+    BW3D = zeros( imgSize(1), imgSize(2), nFiles, 'uint8' );
+else
+    BW3D = false( imgSize(1), imgSize(2), nFiles );
 end
-for i=1:10
-[R,C,Z]=ind2sub(size(BW3D),CC.PixelIdxList{Idx(i)});
-scatter3(R,C,Z,'.')
+
+%{
+Serial-reading of images to assign values in BW3D matrix
+%}
+for iFile=1:nFiles
+    BW3D(:,:,iFile) = imread( [pathName fileName(iFile,:)] );
 end
-clc
-memory
-mem
-pmem
-clc
-CC=bwconncomp(BW,26);
-CC=bwconncomp(BW3D,26);
-[NumVox,Idx]=sort(cellfun(@numel,CC.PixelIdxList),'descend');
-Idx(1)
-Data=[];
-for i=1:10
-[R,C,Z]=ind2sub(size(BW3D),CC.PixelIdxList{Idx(i)});
-Data=[Data;[R,C,Z]]
+
+%{
+Get the number of all voxels and the number of pore voxels
+%}
+numVoxAll = numel(BW3D);
+numVoxPore = sum(sum(sum(BW3D)));
+
+%{
+Delete variables not further used from Workspace
+%}
+clear fileName nFiles iFile img imgSize ans
+
+%{
+Calculate connectivity with the default 26-connectivity
+%}
+CC = bwconncomp(BW3D,26);
+
+%{
+Get the number of voxels in each connected pore network
+ordered from large to small
+%}
+[NumVoxConnPores,Idx] = sort(cellfun(@numel,CC.PixelIdxList),'descend');
+
+%{
+Display some useful information
+%}
+fprintf('Resolution in the direction of Row, Column and Page:\n');
+fprintf('  %d %d %d\n',size(BW3D,1),size(BW3D,2),size(BW3D,3));
+
+fprintf('Ratio of voxels -- 5 largest pore-networks vs. all pores:\n');
+for i = 1:5
+    fprintf('  %5.2f%%', 100 * NumVoxConnPores(i) / numVoxPore);
 end
-clc
-Data=[];
-for i=1:10
-[R,C,Z]=ind2sub(size(BW3D),CC.PixelIdxList{Idx(i)});
-Data=[Data;[R,C,Z]];
+fprintf('\n');
+
+fprintf('Ratio of voxels -- 5 largest pore-networks vs. input domain:\n');
+for i = 1:5
+    fprintf('  %5.2f%%', 100 * NumVoxConnPores(i) / numVoxAll);
 end
-Data=[];
-for i=1:10
-[R,C,Z]=ind2sub(size(BW3D),CC.PixelIdxList{Idx(i)});
-N=ones(size(R))*i;
-Data=[Data;[R,C,Z]];
+fprintf('\n');
+
+%{
+Create a 2D matrix for coloring the pore voxels.
+Each row of the matrix stores (R, C, Z, Color)
+%}
+VOX = zeros(numVoxPore, 4);
+
+%{
+Color the #1 largest pore-network voxels with 1
+%}
+[R,C,Z] = ind2sub( size(BW3D), CC.PixelIdxList{Idx(1)} );
+numStart = 1;
+numEnd = size(R,1);
+for ii = numStart : numEnd
+    iii = ii - numStart + 1;
+    VOX(ii,1) = R(iii);
+    VOX(ii,2) = C(iii);
+    VOX(ii,3) = Z(iii);
+    VOX(ii,4) = 1;
 end
-Data=[];
-for i=1:10
-[R,C,Z]=ind2sub(size(BW3D),CC.PixelIdxList{Idx(i)});
-N=ones(size(R))*i;
-Data=[Data;[R,C,Z,N]];
+fprintf('Voxels of #01 pore-network: %12d\n',iii);
+
+%{
+Color the #2 - #10 largest pore-network voxels from 2 to 10
+%}
+for i = 2 : 10
+    [R,C,Z] = ind2sub( size(BW3D), CC.PixelIdxList{Idx(i)} );
+    numStart = numEnd + 1;
+    numEnd = numEnd + size(R,1);
+    for ii = numStart : numEnd
+        iii = ii - numStart + 1;
+        VOX(ii,1) = R(iii);
+        VOX(ii,2) = C(iii);
+        VOX(ii,3) = Z(iii);
+        VOX(ii,4) = i;
+    end
+    fprintf('Voxels of #%02d pore-network: %12d\n',i,iii);
 end
-size(Data)
-for i=11:CC.NumObjects
-[R,C,Z]=ind2sub(size(BW3D),CC.PixelIdxList{Idx(i)});
-N=ones(size(R))*11;
-Data=[Data;[R,C,Z,N]];
+
+%{
+Color the rest of pore voxels with 11
+%}
+for i = 11 : CC.NumObjects
+    [R,C,Z] = ind2sub( size(BW3D), CC.PixelIdxList{Idx(i)} );
+    numStart = numEnd + 1;
+    numEnd = numEnd + size(R,1);
+    for ii = numStart : numEnd
+        iii = ii - numStart + 1;
+        VOX(ii,1) = R(iii);
+        VOX(ii,2) = C(iii);
+        VOX(ii,3) = Z(iii);
+        VOX(ii,4) = 11;
+    end
 end
-Dil1=imdilate(BW3D,true(3,3,3));
-Surf1=Dil1~=BW3D;
-Dil2=imdilate(Dil1,true(3,3,3));
-Surf2=Dil2~=Dil1;
-imshow(Surf1(;,:,round(end/2));
-imshow(Surf1(:,:,round(end/2));
-imshow(Surf1(:,:,round(end/2)))
-IndS1=find(Surf1);
-IndS2=find(Surf2);
-[R,C,Z]=ind2sub(size(BW3D),IndS1);
-DataS1=[[R,C,Z,zeros(size(R))];Data];
-[R,C,Z]=ind2sub(size(BW3D),IndS2);
-DataS2=[[R,C,Z,zeros(size(R))];Data];
-csvwrite(DataS1.csv, DataS1);
-csvwrite('DataS1.csv', DataS1);
-csvwrite('Data.csv', Data);
+
+%{
+Obsolete
+
+Data = [];
+for i = 1 : 10
+    [R,C,Z] = ind2sub( size(BW3D), CC.PixelIdxList{Idx(i)} );
+    N = ones(size(R)) * i;
+    Data = [Data; [R,C,Z,N]];
+    msg = sprintf('Voxels of #%02d pore-network: %8d',i,size(N,1));
+    disp(msg);
+end
+
+for i = 11 : CC.NumObjects
+    [R,C,Z] = ind2sub( size(BW3D), CC.PixelIdxList{Idx(i)} );
+    N = ones(size(R)) * 11;
+    Data = [Data; [R,C,Z,N]];
+end
+%}
+
+tStart = cputime;
+outFileName = "vox_pore.txt";
+fprintf('Export file %s ... ', outFileName);
+fileID = fopen(outFileName,'w');
+for i = 1 : numVoxPore
+    fprintf(fileID,'%g %g %g %g\n',VOX(i,1),VOX(i,2),VOX(i,3),VOX(i,4));
+end
+fclose(fileID);
+tElapsed = cputime - tStart;
+fprintf('CPU time = %.3f\n', tElapsed);
+
+tStart = cputime;
+fprintf('Dilate the pore voxels with direct neighbor solid voxels... ');
+DIL1 = imdilate(BW3D,true(3,3,3));
+tElapsed = cputime - tStart;
+fprintf('CPU time = %.3f\n', tElapsed);
+
+tStart = cputime;
+fprintf('Get the surface wall voxels that encolse the pores... ');
+SURF1 = DIL1 ~= BW3D;
+tElapsed = cputime - tStart;
+fprintf('CPU time = %.3f\n', tElapsed);
+clear DIL1
+
+tStart = cputime;
+fprintf('Get the indices of all non-zero entries... ');
+indSURF1 = find(SURF1);
+tElapsed = cputime - tStart;
+fprintf('CPU time = %.3f\n', tElapsed);
+clear SURF1
+
+tStart = cputime;
+fprintf('Convert the non-zero entry indices to I,J,K indexing... ');
+[R,C,Z] = ind2sub(size(BW3D),indSURF1);
+tElapsed = cputime - tStart;
+fprintf('CPU time = %.3f\n', tElapsed);
+clear BW3D indSURF1
+
+tStart = cputime;
+fprintf('Append the boundary wall voxels into the pore voxel matrix... ');
+VOX = [[R,C,Z,zeros(size(R))]; VOX];
+tElapsed = cputime - tStart;
+fprintf('CPU time = %.3f\n', tElapsed);
+
+tStart = cputime;
+outFileName = "vox_wall_pore.txt";
+fprintf('Export file %s ... ', outFileName);
+fileID = fopen(outFileName,'w');
+numVoxWallPore = size(VOX,1);
+for i = 1 : numVoxWallPore
+    fprintf(fileID,'%g %g %g %g\n',VOX(i,1),VOX(i,2),VOX(i,3),VOX(i,4));
+end
+fclose(fileID);
+tElapsed = cputime - tStart;
+fprintf('CPU time = %.3f\n', tElapsed);
+
+%Dil2 = imdilate(Dil1,true(3,3,3));
+%Surf2=Dil2~=Dil1;
